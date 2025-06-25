253a1347572bd7e57f6f1d021cbbca83c6b39104
Anatoliy_Noscov <svodnuy7777@gmail.com>
2025-06-25 16:08:18 +0300

    23

--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -2,22 +2,22 @@ import React from "react";
 import css from "./SearchBox.module.css";
 
 interface SearchBoxProps {
-  value: string;
   onChange: (query: string) => void;
+  value: string;
 }
 
 export default function SearchBox({ value, onChange }: SearchBoxProps) {
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    onChange(event.target.value);
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    onChange(e.target.value);
   }
 
   return (
     <input
       className={css.input}
-      value={value}
-      onChange={handleChange}
       type="text"
       placeholder="Search notes"
+      value={value}
+      onChange={handleChange}
     />
   );
 }
